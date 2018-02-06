ac27dc24121b6239e76757651b9313e999cfb6f2
Filip Hracek <filip.hracek@gmail.com>
2018-02-05 17:37:33 -0800

    Add saving functionality and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
@@ -20,26 +17,53 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+class NameWidget extends StatelessWidget {
+  final WordPair _pair;
+
+  final bool _saved;
+
+  final GestureTapCallback _onTap;
+
+  NameWidget(this._pair, this._saved, this._onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    final richText = new RichText(
+      text: new TextSpan(
+          children: <TextSpan>[
+            new TextSpan(
+              text: _pair.first.toLowerCase(),
+              style: new TextStyle(color: Theme.of(context).primaryColor),
+            ),
+            new TextSpan(text: _pair.second.toLowerCase())
+          ],
+          style: new TextStyle(
+              color: Colors.black54,
+              fontSize: 24.0,
+              fontFamily: 'Gloria Hallelujah')),
+    );
+    return new ListTile(
+      leading: new Icon(
+        _saved ? Icons.star : Icons.star_border,
+        size: 24.0,
+      ),
+      title: richText,
+      onTap: _onTap,
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  final List<WordPair> _pairs = [];
-
-  void initState() {
-    super.initState();
-    _getNewNames();
-  }
-
-  void _getNewNames() {
-    final newNames = generateWordPairs().take(10);
-    _pairs.addAll(newNames);
-  }
+  final List<WordPair> _suggestions = [];
+  final Set<WordPair> _saved = new Set();
 
   @override
   Widget build(BuildContext context) {
@@ -47,23 +71,33 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: new ListView.builder(itemBuilder: (context, index) {
-        while (index >= _pairs.length) {
-          _getNewNames();
-        }
-        return new NameWidget(_pairs[index]);
-      }),
+      body: _buildSuggestions(),
     );
   }
-}
 
-class NameWidget extends StatelessWidget {
-  final WordPair _pair;
+  void initState() {
+    super.initState();
+    _getNewNames();
+  }
 
-  NameWidget(this._pair);
+  ListView _buildSuggestions() {
+    return new ListView.builder(itemBuilder: (context, index) {
+      while (index >= _suggestions.length) {
+        _getNewNames();
+      }
+      final pair = _suggestions[index];
+      return new NameWidget(pair, _saved.contains(pair), () {
+        if (_saved.contains(pair)) {
+          setState(() => _saved.remove(pair));
+          return;
+        }
+        setState(() => _saved.add(pair));
+      });
+    });
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return new Text(_pair.toString());
+  void _getNewNames() {
+    final newNames = generateWordPairs().take(10);
+    _suggestions.addAll(newNames);
   }
 }
